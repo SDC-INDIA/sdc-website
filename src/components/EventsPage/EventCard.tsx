673f0e65c3d9aeb0e03ee21c4dcbed187b313d99
SiddharthKoyugra <koyugurasiddhardha@gmail.com>
2023-10-30 02:01:34 +0530

    design update

--- src/components/EventsPage/EventCard.tsx
+++ src/components/EventsPage/EventCard.tsx
@@ -16,7 +16,7 @@ const EventCard = (props: any) => {
             // whileInView={{ opacity: 1, x: 0 }}
             // exit={{ opacity: 0, x: "-50%" }}
         >
-            <div className="flex flex-col justify-center flex-center">
+            <div className="flex flex-col max-md:mx-auto justify-center flex-center">
                 <div className="relative md:w-[500px] md:h-[300px] w-[300px] h-[200px] mx-auto">
                     <Image src={image} fill alt="" className="object-cover rounded-lg shadow-md" />
                 </div>
@@ -24,7 +24,7 @@ const EventCard = (props: any) => {
             <div className="flex justify-center md:w-[40%] w-fit mx-auto">
                 <div className="flex flex-col justify-center my-8 text-center" >
                     <h1 className="text-[1.5rem] custom-color-text font-bold">{title}</h1>
-                    <p className="text-[1.2rem] text-black text-justify">{description}</p>
+                    <p className="text-[1rem] text-black text-justify">{description}</p>
                     {/* <p className="text-[1rem] text-black">{date}</p> */}
                     {
                         user && <button onClick={() => deleteEvent(id)} className="my-4 bg-orange-500 hover:bg-orange-400 rounded py-2 px-4 w-fit text-white mx-auto">Delete</button>
